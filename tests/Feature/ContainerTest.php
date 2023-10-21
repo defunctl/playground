52675c004260d63360912df2d1d8aa3374830d9b
Justin Frydman <justin.frydman@gmail.com>
2023-10-21 14:51:20 -0600

    Add php-di container for tests, add Feature test suite, add composer test:unit, composer test:feature

---
+++ tests/Feature/ContainerTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Calculator;
+use Psr\Container\ContainerInterface;
+use Tests\TestCase;
+
+final class ContainerTest extends TestCase {
+
+	public function test_it_gets_correct_instances(): void {
+		$this->assertInstanceOf( ContainerInterface::class, $this->container );
+		$this->assertInstanceOf( Calculator::class, $this->container->get( Calculator::class ) );
+	}
+
+	public function test_it_sets_instances(): void {
+		$calculator = new Calculator();
+
+		$this->assertNotSame( $calculator, $this->container->get( Calculator::class ) );
+
+		$this->container->set( Calculator::class, $calculator );
+
+		$this->assertSame( $calculator, $this->container->get( Calculator::class ) );
+	}
+
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use DI\Container;
 use Mockery;
 
 class TestCase extends \PHPUnit\Framework\TestCase {
+
+	protected Container $container;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->container = new Container();
+	}
 
 	protected function tearDown(): void {
 		Mockery::close();
